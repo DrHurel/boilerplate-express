084e190aee1319edda33ac172a0453f2fe50a33a
DrHurel <jeremyhurel34@gmail.com>
2023-07-18 03:07:55 +0200

    wip freecodecamp course

--- myApp.js
+++ myApp.js
@@ -1,16 +1,37 @@
 let express = require('express');
 let app = express();
 
+//Serve static assets
 
-console.log("Hello World");
+app.use(function (req, res, next) {
+  console.log(req.method + ' ' + req.path + ' - ' + req.ip);
+  next();
+});
+
+app.use('/public', express.static(__dirname + '/public'));
+
+
+//GET
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/json', function (req, res) {
+  res.json({ "message": "Hello json" });
+});
+
+app.get('/now', function (req, res, next) {
+  req.time = new Date().toString();
+  next();
+}, function (req, res) {
+  res.json({ "time": req.time });
+});
 
 
 
-
-
-
-
-
+app.listen(4444, function () {
+  console.log('Listening on port 4444');
+});
 
 
 
